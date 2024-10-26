6f6381b20de80afca20a37019020c85530406f41
rafaelswr <rafaels.borges91@gmail.com>
2024-10-26 23:55:54 +0100

    set up spring project

---
+++ src/main/java/com/rafaelswr/springbootreactivedemo/SpringBootReactiveDemoApplication.java
@@ -0,0 +1,13 @@
+package com.rafaelswr.springbootreactivedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReactiveDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootReactiveDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rafaelswr/springbootreactivedemo/SpringBootReactiveDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaelswr.springbootreactivedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootReactiveDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
